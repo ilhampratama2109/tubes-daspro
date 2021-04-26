ce67d27ad2b1c7ac6055051c3065ab70d5ace617
ilhampratama2109 <pilham902@gmail.com>
2021-04-26 09:51:46 +0700

    dibagian loopnya ada yg kuubah

--- search.py
+++ search.py
@@ -13,20 +13,23 @@ def split_csv(string, delimiters=";\n"):
 
 
 def search_by_rarity(filename):
-    with open("consumamble.csv", "r") as data:
+    with open(filename, "r") as data:
         print(">>> mencari berdasarkan rarity")
         rarity_input = input("Masukkan rarity : ")
         length = 0
         results = []
+        count = 0
         for line in data:
             words = split_csv(line)
             results.append((words[0:]))
             length += 1
+        
         for i in range(0, length):
             if results[i][4] == rarity_input:
                 print(results[i])
-            else:
-                print("rarity tidak terdaftar")
+                count = count +1
+        if (count == 0):
+            print("ID tidak ditemukan")
 
 def search_by_year(filename):
     with open(filename, "r") as data:
@@ -35,36 +38,48 @@ def search_by_year(filename):
         kategori = input("masukkan kategori : ")
         length = 0
         results = []
+        count = 0
         for line in data:
             words = split_csv(line)
             results.append((words[0:]))
             length += 1
+
         for i in range(0, length):
             if (kategori == ("=")):
                 if (results[i][5] == year_input):
                     print(results[i])
+                    count = count + 1
                 else:
-                    print("Tidak ada gadget yang ditemukan")
+                    continue
             elif (kategori == (">")):
                 if (results[i][5] > year_input):
                     print(results[i])
+                    count = count + 1
                 else:
-                    print("Tidak ada gadget yang ditemukan")
+                    continue
             elif (kategori == ("<")):
                 if (results[i][5] < year_input):
                     print(results[i])
+                    count = count + 1
                 else:
-                    print("Tidak ada gadget yang ditemukan")
+                    continue
             elif (kategori == (">=")):
                 if (results[i][5] >= year_input):
                     print(results[i])
+                    count = count + 1
                 else:
-                    print("Tidak ada gadget yang ditemukan")
-            else: #kategori <=
+                    continue
+            elif (kategori == ("<=")):
                 if (results[i][5] <= year_input):
                     print(results[i])
+                    count = count + 1
                 else:
-                    print("Tidak ada gadget yang ditemukan")
+                    continue
+            else:
+                continue
+        if (count == 0):
+            print("tidak ada data yang ditemukan")
+                
 
 def update_data(filename):
     with open(filename, "w") as data:
@@ -72,6 +87,7 @@ def update_data(filename):
         id_input = input("Masukkan ID : ")
         length = 0
         results = []
+        count = 0
         for line in data:
             words = split_csv(line)
             results.append((words[0:]))
@@ -80,5 +96,8 @@ def update_data(filename):
             if results[i][0] == id_input:
                 for j in range(len(results[i])):
                     results[i][j]= input()
+                    count = count + 1
             else:
-                print("rarity tidak terdaftar")
+                continue
+        if (count == 0):
+            print("tidak ada data yang ditemukan")
